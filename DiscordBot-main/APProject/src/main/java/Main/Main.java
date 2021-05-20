86bdcca16a0abbb46ffda757791b7951cde9be83
Luca Andreea <54581547+andreealuca25@users.noreply.github.com>
2021-05-20 20:09:03 +0300

    Update Main.java

--- DiscordBot-main/APProject/src/main/java/Main/Main.java
+++ DiscordBot-main/APProject/src/main/java/Main/Main.java
@@ -34,5 +34,6 @@ public class Main {
     public static void registerListeners() {
         builder.addEventListeners(new Clear());
         builder.addEventListeners(new Info());
+        builder.addEventListeners(new Mute());
     }
 }
