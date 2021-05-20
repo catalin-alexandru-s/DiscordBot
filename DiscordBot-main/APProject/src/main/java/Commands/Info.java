56c77e947c5b250e669bcc900268e851ef45a708
andreealuca25 <lucaandreearaluca@yahoo.com>
2021-05-20 12:51:13 +0300

    move packages

---
+++ DiscordBot-main/APProject/src/main/java/Commands/Info.java
@@ -0,0 +1,26 @@
+package Commands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import Main.Main;
+
+public class Info extends ListenerAdapter {
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        String[] args=event.getMessage().getContentRaw().split(" ");
+        if(args[0].equalsIgnoreCase(Main.prefix + "info")){
+            EmbedBuilder info= new EmbedBuilder();
+            info.setTitle("Commands available:");
+            info.setDescription("~clear integer\n~trivia");
+            info.setColor(0x9e1fff);
+
+            event.getChannel().sendTyping().queue();
+            event.getChannel().sendMessage(info.build()).queue();
+            info.clear();
+        }
+
+    }
+
+}
+
+
