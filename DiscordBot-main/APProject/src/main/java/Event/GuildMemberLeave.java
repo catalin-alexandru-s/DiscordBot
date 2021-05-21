54b92ab669e48feda963294f3d7e095faddf7be6
andreealuca25 <lucaandreearaluca@yahoo.com>
2021-05-21 14:54:47 +0300

    join & leave events

---
+++ DiscordBot-main/APProject/src/main/java/Event/GuildMemberLeave.java
@@ -0,0 +1,26 @@
+package Event;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.awt.*;
+import java.util.Random;
+
+public class GuildMemberLeave extends ListenerAdapter {
+    String[] messages = {
+            "[member] left. bye bye fraere",
+
+    };
+    public void onGuildMemberRemove(GuildMemberRemoveEvent event) throws NullPointerException
+    {
+        Random rand= new Random();
+        int number= rand.nextInt(messages.length);
+
+        EmbedBuilder leave=new EmbedBuilder();
+        leave.setColor(Color.MAGENTA);
+        leave.setDescription(messages[number].replace("[member]", event.getUser().getAsMention()));
+
+        event.getGuild().getDefaultChannel().sendMessage(leave.build()).queue();
+    }
+}
